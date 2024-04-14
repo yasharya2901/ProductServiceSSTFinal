a9cf72cf2b0a1a04275171a14aa3252bcbbe837d
yasharya2901 <yasharya2901@gmail.com>
2024-04-14 12:57:00 +0530

    Created Tables of the Models Cardinality Relation between two tables
    Product ------------ Category
    1 --> 1
    Many <-- 1
    The relation between Product and Category is Many:1
    
    We made a class a table by using the annotation @Entity and made an attribute a primary key by using @Id annotation.
    Also, we inserted a foreign key using the annotation @ManyToOne, essentially the annotation describing the cardinality between the two tables.

--- src/main/java/com/yasharya2901/github/productservicesstfinal/models/Category.java
+++ src/main/java/com/yasharya2901/github/productservicesstfinal/models/Category.java
@@ -1,13 +1,17 @@
 package com.yasharya2901.github.productservicesstfinal.models;
 
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Category {
+    @Id
     private Long id;
     private String title;
     private String description;
--- src/main/java/com/yasharya2901/github/productservicesstfinal/models/Product.java
+++ src/main/java/com/yasharya2901/github/productservicesstfinal/models/Product.java
@@ -1,17 +1,27 @@
 package com.yasharya2901.github.productservicesstfinal.models;
 
 
+import com.fasterxml.jackson.databind.annotation.EnumNaming;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Product {
+    @Id
     private Long id;
     private String title;
     private String description;
     private Double price;
+    @ManyToOne
     private Category category;
     private String img;
 
